565afdd2ca14d7c56d74eee5c7382b9b2a4469fd
Travis Simpson <travis.simpson92@hotmail.com>
2019-02-05 16:08:57 -0500

    README update

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'typeface-roboto';
+import Album from './album/Album';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <Album />
+    );
+  }
+}
+
+export default App;
